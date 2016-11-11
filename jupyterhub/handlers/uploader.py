f7605560a122857f6cef80de3aabc984955e85c1
Martin Jones <m.jones@ed.ac.uk>
2016-11-11 17:34:01 +0000

    Add server side validation and message handling

--- jupyterhub/handlers/uploader.py
+++ jupyterhub/handlers/uploader.py
@@ -6,7 +6,7 @@ from .base import BaseHandler
 from .login import LoginHandler
 import os, uuid, re
 
-__UPLOADS__ = "../uploads/"
+__UPLOADS__ = os.path.expanduser('~') + "/uploads/"
 
 
 class DataImportHandler(BaseHandler):
@@ -20,25 +20,44 @@ class DataImportHandler(BaseHandler):
 
     @gen.coroutine
     def post(self):
-        fileinfo = self.request.files['filearg'][0]
-        print("fileinfo is {0}".format(fileinfo))
-        fname = fileinfo['filename']
-        filename, file_extension = os.path.splitext(fname)
-        print("filename is {0}".format(filename))
-        print("extn is {0}".format(file_extension))
-        newname = (re.sub('[^\w\s-]', '', filename).strip().lower())
-        newname = (re.sub('[-\s]+', '-', newname))
-        cname = newname + file_extension
-        print("cname is {0}".format(cname))
-        fh = open(__UPLOADS__ + cname, 'wb')
-        fh.write(fileinfo['body'])
-        self.write(cname + " was successfully uploaded.")
+        file_uploaded = False
+        message = ''
+        error_message = ''
+        message_class = ''
+        form_class = ''
 
+        if len(self.request.files) != 0:
+            message = '<h3>Summary</h3>'
+            fileinfo = self.request.files['filearg'][0]
+            fname = fileinfo['filename']
+            filename, file_extension = os.path.splitext(fname)
+            newname = (re.sub('[^\w\s-]', '', filename).strip().lower())
+            newname = (re.sub('[-\s]+', '-', newname))
+            cname = newname + file_extension
+            fh = open(__UPLOADS__ + cname, 'wb')
+            fh.write(fileinfo['body'])
+            messages = cname + " was successfully uploaded to " + __UPLOADS__ + cname +". Info is {}".format(message)
+            message_class = 'bg-success'
+        else:
+            messages = 'Oops, there was a problem...'
+            error_message = 'You forgot to select a file'
+            message_class = 'bg-danger'
+            form_class = 'has-error'
+
+        html = self.render_template('fileupload.html',
+        user=self.get_current_user(),
+        messages = messages,
+        error_message = error_message,
+        message_class = message_class,
+        form_class = form_class
+        )
+        self.finish(html)
 
 class HelloHandler(BaseHandler):
     def get(self):
         user = self.get_current_user()
-        self.write("Hello {0}".format(user.name))
+        userhome = os.path.expanduser('~')
+        self.write("Hello {0}. Your home is {1}".format(user.name,userhome))
 
 
 default_handlers = [
